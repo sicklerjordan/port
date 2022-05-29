8ea044630cb00451ef011534ae7a026134f58e84
Jordan Mathew Sickler <91163847+sicklerjordan@users.noreply.github.com>
2022-05-28 22:16:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Side from './components/Side'
+import Contact from './components/Contact'
+import About from './components/About'
+import Hero from './components/Hero'
+import Intro from './components/Intro';
+import Project from './components/Project'
+import useLocalStorage from 'use-local-storage';
+import {BsToggleOff} from 'react-icons/bs'
+import {BsToggleOn} from 'react-icons/bs'
+
+
+
+
 
 function App() {
+
+  const [theme, setTheme] = useLocalStorage('theme' ? 'dark' : 'light')
+
+  const switchTheme = () => {
+      const newTheme = theme === 'light' ? 'dark' : 'light';
+      setTheme(newTheme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+
+{theme === 'light' ? <div className='toggle-mode'>CHANGE THEME</div> : <div className='toggle-mode'>CHANGE THEME</div>} 
+
+{theme === 'light' ? <BsToggleOff onClick={switchTheme} className='toggle'/> : <BsToggleOn onClick={switchTheme} className='toggle'/>} 
+
+  {/*   <Intro />*/}
+      <Navbar /> 
+      <Project />
+      {/*<Hero />*/}
+     {/* <About />*/}
+      
+      <Contact />
+     <Side />
     </div>
   );
 }
