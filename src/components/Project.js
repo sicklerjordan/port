ce3a5c17a0b8746553c90177378c80cff1ee99f9
Jordan Mathew Sickler <91163847+sicklerjordan@users.noreply.github.com>
2022-09-18 14:35:29 -0400

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,17 @@
 
 import React from 'react';
 import ss from './images/ss.png'
-import rick from './images/rick.png'
 import crypto from './images/crypto-site.png'
 import admin from './images/admin.png'
 import reddit from './images//reddit.png'
 import color from './images/color.png'
 import kkvid from './images/kkvid.mp4'
-import rickVid from './images/rickVid.mov'
 import cryptoVid from './images/cryptoVid.mov'
 import adocVid from './images/adocVid.mov'
 import redditVid from './images/redditVid.mov'
 import colorVid from './images/colorVid.mov'
+import trinityVid from './images/trinityVid.mov'
+import stockVid from './images/stockVid.mov'
 
 
 
@@ -29,52 +29,66 @@ function Work() {
     
   return (
     <div data-aos='fade-in'>
-        <h1 id="work"className='project-header'>SOME OF MY WORK</h1>
-        <a href='https://github.com/sicklerjordan' target="_blank" id="sub"><p className='project-sub'>GITHUB</p></a>
 
+<h1 id="work"className='project-header'>SOME OF MY WORK</h1>
+        <a href='https://github.com/sicklerjordan' target="_blank" id="sub"><p className='project-sub'>GITHUB</p></a>
         <div className='project-container'>
         <div className='card'>
+            <a href="">
                 <video muted autoStart autoPlay loop>
                 <source autoStart autoPlay  type="video/mp4" src={cryptoVid}/>
                 </video>
+                </a>
             </div> 
             <div className='card'>
+                <a>
                 <video muted autoStart autoPlay loop>
                 <source autoStart autoPlay  type="video/mp4" src={adocVid}/>
                 </video>
+                </a>
             </div> 
             <div className='card'>
-                <img src={ss} alt='' />
-            </div>
-            <div className='card'>
-                <video muted autoStart autoPlay loop>
-                <source autoStart autoPlay  type="video/mp4" src={adocVid}/>
-                </video>
-            </div> 
-            <div className='card'>
-                <video muted autoStart autoPlay loop>
-                <source autoStart autoPlay  type="video/mp4" src={rickVid}/>
-                </video>
-            </div> 
-            <div className='card'>
-                <video muted autoStart autoPlay loop>
-                <source autoStart autoPlay  type="video/mp4" src={rickVid}/>
-                </video>
-            </div>
-            <div className='card'>
+                <a>
             <video muted autoStart autoPlay loop>
                 <source autoStart autoPlay  type="video/mp4" src={colorVid}/>
                 </video>
+                </a>
             </div>
             <div className='card'>
-            <video muted autoStart autoPlay loop>
-                <source autoStart autoPlay  type="video/mp4" src={redditVid}/>
-                </video>
-            </div>
-            <div className='card'>
+                <a href="https://github.com/sicklerjordan/crypto-app">
                 <video muted autoStart autoPlay loop>
+                <source autoStart autoPlay  type="video/mp4" src={stockVid}/>
+                </video>
+                </a>
+            </div> 
+            <div className='card'>
+                <a href="">
+                <video muted autoStart autoPlay loop>
+                <source autoStart autoPlay  type="video/mp4" src={trinityVid}/>
+                </video>
+                </a>
+            </div> 
+            
+            <div className='card'>
+                <a href="">
+            <video muted autoStart autoPlay loop>
+                <source autoStart autoPlay  type="video/mp4" src={colorVid}/>
+                </video>
+                </a>
+            </div>
+            <div className='card'>
+                <a href="https://github.com/sicklerjordan/subreddit">
+            <video muted autoStart autoPlay loop>
+                <source autoStart autoPlay  type="video/mp4" src={redditVid}></source>
+                </video>
+                </a>
+            </div> 
+            <div className='card'>
+                <a href="">
+                    <video muted autoStart autoPlay loop>
                 <source autoStart autoPlay  type="video/mp4" src={kkVid}/>
                 </video>
+                </a>
             </div> 
         </div>
     </div>
